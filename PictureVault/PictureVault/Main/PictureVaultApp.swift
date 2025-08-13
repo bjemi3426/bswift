45866a9e0628b42ecaf882b15c4ec8e9dee297b3
bjemi3426 <gotime1254@icloud.com>
2025-08-13 17:23:29 -0400

    Add files via upload

---
+++ PictureVault/PictureVault/Main/PictureVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureVaultApp.swift
+//  PictureVault
+//
+//  Created by Bjem on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PictureVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
