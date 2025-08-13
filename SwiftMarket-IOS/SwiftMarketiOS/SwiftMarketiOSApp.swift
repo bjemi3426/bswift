825541eff1bfaaf4b407687a7afab33844c70f84
bjemi3426 <gotime1254@icloud.com>
2025-08-13 18:05:37 -0400

    Add files via upload

---
+++ SwiftMarket-IOS/SwiftMarketiOS/SwiftMarketiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMarketiOSApp.swift
+//  SwiftMarketiOS
+//
+//  Created by Brandon Jemison on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMarketiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
